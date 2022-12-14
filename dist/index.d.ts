2233a922c59775156e3689a9d20fa979afbc56f9
Jonathon Hill <jonathon@compwright.com>
2022-12-14 09:00:32 -0500

    feat: add typescript type definitions

---
+++ dist/index.d.ts
@@ -0,0 +1,31 @@
+declare class MetadataReader {
+    constructor(metadata: any, options?: {});
+    get identifierFormat(): any;
+    get identityProviderUrl(): any;
+    get logoutUrl(): any;
+    get encryptionCerts(): any;
+    get encryptionCert(): any;
+    get signingCerts(): any;
+    get signingCert(): any;
+    get claimSchema(): any;
+    #private;
+}
+
+declare function claimsToCamelCase(claims: any, claimSchema: any): {};
+
+declare function toPassportConfig(reader?: {}, options?: {
+    multipleCerts: boolean;
+}): {
+    identityProviderUrl: any;
+    entryPoint: any;
+    logoutUrl: any;
+    cert: any;
+    identifierFormat: any;
+};
+
+declare function _default$1(config: any): () => void;
+
+declare function fetchMetadata(config?: {}): Promise<any>;
+declare function _default(config: any): Promise<MetadataReader>;
+
+export { MetadataReader, claimsToCamelCase, _default as fetch, fetchMetadata, _default$1 as metadata, toPassportConfig };
